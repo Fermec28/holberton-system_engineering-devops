0d22a60608989aac28a4e62b1a542cc5b8502f9f
Jose Medina <fermed28@gmail.com>
2019-08-05 21:06:15 +0000

    create file with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# create file in /tmp directory
+file { '/tmp/holberton':
+        mode    => '0744',
+        owner   => 'www-data',
+        group   => 'www-data',
+        content => 'I love Puppet'
+    }
