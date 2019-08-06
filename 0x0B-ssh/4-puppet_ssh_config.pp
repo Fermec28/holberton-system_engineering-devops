f951dd5dc2a7be35553b004f4f73f7f157309d06
Jose Medina <fermed28@gmail.com>
2019-08-06 19:31:03 +0000

    [add] config with puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,16 @@
-#!/usr/bin/env bash
+# modify ssh config with puppet
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+file_line { 'change auth type':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication.*$',
+}
+file_line { 'set identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => '^IdentityFile.$',
+}
