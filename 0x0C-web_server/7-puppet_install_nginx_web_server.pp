de1c7cba10f609cd4511fd373f227d221e343650
Jose Medina <fermed28@gmail.com>
2019-08-08 02:05:54 +0000

    add puppet with nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+#install nginx server
+
+  package { 'apache2.2-common':
+    ensure => absent,
+  }
+  package { 'nginx':
+    ensure  => installed,
+    require => Package['apache2.2-common'],
+  }
+  service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+  }
