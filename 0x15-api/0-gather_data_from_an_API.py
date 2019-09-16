493c007bef79b0aa44768707f82977343815e639
Jose Medina <fermed28@gmail.com>
2019-09-16 22:21:47 +0000

    [fixes] adding decimal

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
                          params=payload)
     done = list(filter(lambda x: x.get("completed") is True, tasks.json()))
-    print("Employee {} is done with tasks({}/{}):".format(user_name,
+    print("Employee {} is done with tasks({:d}/{:d}):".format(user_name,
           len(done), len(tasks.json())))
     for task_done in done:
         print("\t {}".format(task_done.get("title")))
