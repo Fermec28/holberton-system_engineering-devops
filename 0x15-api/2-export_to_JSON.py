4dc9fde81fd9f88b7c4ba2aa1d484594bdb05372
Jose Medina <fermed28@gmail.com>
2019-09-16 23:33:51 +0000

    [add] write json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+""" Export to CSV """
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+    import json
+
+    payload = {'userId': argv[1]}
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(argv[1])).json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
+                         params=payload).json()
+    list_task = []
+    for task in tasks:
+        list_task.append({"task": task.get("title"),
+                          "completed": task.get("completed"),
+                          "username": user.get("username")})
+    with open('{}.json'.format(argv[1]), 'w') as f:
+        json.dump({argv[1]: list_task}, f)
