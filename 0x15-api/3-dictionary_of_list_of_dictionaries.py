752f6be2492fd7820e2358986d23eef42ad74b25
Jose Medina <fermed28@gmail.com>
2019-09-16 23:58:47 +0000

    [add] all user tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+""" Export to CSV """
+if __name__ == "__main__":
+    import csv
+    import requests
+    import json
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    data = {}
+    for user in users:
+        payload = {'userId': user.get("id")}
+        tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
+                             params=payload).json()
+        list_task = []
+        for task in tasks:
+            list_task.append({"task": task.get("title"),
+                              "completed": task.get("completed"),
+                              "username": user.get("username")})
+        data[user.get("id")] = list_task
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(data, f)
