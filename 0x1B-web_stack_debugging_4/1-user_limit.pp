7e3726fa132546b4a4a14bbb8c21e5ee1861de29
Jose Medina <fermed28@gmail.com>
2019-10-10 23:19:46 +0000

    [add] solution

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash
+#change limit
+exec { 'change hard':
+    command  => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1001/'\
+    	     	 /etc/security/limits.conf",
+    provider => shell
+}
+-> exec { 'change hard':
+    command  => "sed -i 's/holberton soft nofile 4/holberton soft nofile 1001/'\
+    	     	 /etc/security/limits.conf",
+    provider => shell
+}
+-> exec { 'restart':
+  command  => 'sysctl -p',
+  provider => shell
+}
