4685ff31b77094345dd807714194b329013a231e
Jose Medina <fermed28@gmail.com>
2019-10-10 21:44:08 +0000

    [add] puppet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+exec { 'change limit request':
+    path     => '/etc/default/nginx',
+    command  => "echo 'ULIMIT=\"-n 4096\"'> /etc/default/nginx",
+    provider => shell
+}
+-> exec { 'Restart nginx':
+    command  => 'service nginx restart',
+    provider => shell
+}
